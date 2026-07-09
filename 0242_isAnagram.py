a35c76b583875dbe8f817e2372e4aa20e671f27b
liuchang <liuchang_shishiwu@163.com>
2026-07-09 16:59:12 +0800

    commit leetcode 242

---
+++ 0242_isAnagram.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        result_s = {}
+        result_t = {}
+        for ss in s:
+            if ss not in result_s:
+                result_s[ss] = 1
+            else:
+                result_s[ss] += 1
+        for tt in t:
+            if tt not in result_t:
+                result_t[tt] = 1
+            else:
+                result_t[tt] += 1
+
+        unique = set(s)
+
+        for uu in unique:
+            if uu not in result_t or result_s[uu] != result_t[uu]:
+                return False
+        return True
